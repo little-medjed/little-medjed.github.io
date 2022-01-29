e550df560cd1c8231edba6b1196ed78866cec805
Erika <erikamota@protonmail.com>
2022-01-29 18:20:18 -0300

    add favicon and the calculator project

---
+++ projects/calculator/calc.js
@@ -0,0 +1,26 @@
+console.log("丂卂Ҝㄩ尺卂 匚卂ㄥ匚ㄩㄥ卂ㄒㄖ尺");
+
+function insert(x){
+  let num = document.getElementById('calc-screen').innerHTML;
+  document.getElementById('calc-screen').innerHTML = num + x;
+}
+
+function clean(){
+  document.getElementById('calc-screen').innerHTML = "";
+}
+
+function del(){
+  let result = document.getElementById('calc-screen').innerHTML;
+  document.getElementById('calc-screen').innerHTML = result.substring(0, result.length - 1);
+}
+
+function calc(){
+  let result = document.getElementById('calc-screen').innerHTML;
+  if (result) {
+    document.getElementById('calc-screen').innerHTML = eval(result);
+  }
+  else{
+    document.getElementById('calc-screen').innerHTML = "";
+  }
+
+}
